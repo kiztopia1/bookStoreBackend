01d46a196c3ea3b7e3456728e2beb5d680da7c3b
kirubel <kiztopia.2020@gmail.com>
2022-01-25 17:25:41 +0300

    new: book app with model created

--- app/store/models.py
+++ app/store/models.py
@@ -1,3 +1,12 @@
+from tkinter import image_names
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id = models.CharField(primary_key=True, max_length=100)
+    name = models.CharField(max_length=100, default='null')
+    description =models.TextField(default='null')
+    image_name = models.CharField(max_length=100, default='null')
+
+    def __str__(self) :
+        return self.name
