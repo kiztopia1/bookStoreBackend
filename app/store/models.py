13ec81c6a048afad512fce01bae59de480ec432c
kirubel <kiztopia.2020@gmail.com>
2022-01-26 15:24:34 +0300

    new: transaction model created

--- app/store/models.py
+++ app/store/models.py
@@ -1,12 +1,20 @@
+from datetime import date
 from tkinter import image_names
 from django.db import models
 
 # Create your models here.
 class Book(models.Model):
     id = models.CharField(primary_key=True, max_length=100)
-    name = models.CharField(max_length=100, default='null')
+    title = models.CharField(max_length=100, default='null')
+    author = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
     description =models.TextField(default='null')
     image_name = models.CharField(max_length=100, default='null')
 
     def __str__(self) :
-        return self.name
+        return self.title
+
+class Transaction(models.Model):
+    date = models.DateTimeField()
+    bookId = models.ForeignKey(Book, on_delete=models.CASCADE)
+    userId = models.CharField(max_length=100)
